95f9e04d744d9a84dfb058a19ea144740eecd665
Thomas-qwe <146857012+Thomas-qwe@users.noreply.github.com>
2023-11-17 20:16:25 +0300

    Details.java

---
+++ Details.java
@@ -0,0 +1,13 @@
+public class Details{
+  public static void main(String[]args){
+
+      System.out.println("Group Members");
+
+ System.out.println("Jim Githaiga");
+ System.out.println("Brett Mugambi");
+ System.out.println("Harriet Wambui");
+ System.out.println("Fidelis Wandia");
+ System.out.println("Stephen Mwangi");
+
+}
+}
